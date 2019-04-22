310fd1ecac345be2894849c8be0adf2a0d39837f
andela-sakande <surajudeen.akande@andela.com>
2019-04-22 21:25:11 +0100

    use custom hooks

--- src/Game.js
+++ src/Game.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import PropTypes from 'prop-types';
 import Logic from './utils/logic';
@@ -6,13 +6,51 @@ import Balls from './components/Balls';
 import PlayDigit from './components/PlayDigit';
 import PlayAgain from './components/PlayAgain';
 
-const Game = ({ startNewGame }) => {
+const useGameState = timeLimit => {
   const [balls, setBalls] = useState(Logic.random(1, 9));
   const [availableDigits, setAvailableDigits] = useState(Logic.range(1, 9));
   const [candidateDigits, setCandidateDigits] = useState([]);
+  const [secondsLeft, setSecondsLeft] = useState(15);
+
+  useEffect(() => {
+    if (secondsLeft > 0 && availableDigits.length > 0) {
+      const timerId = setTimeout(() => {
+        setSecondsLeft(secondsLeft - 1);
+      }, 1000);
+
+      return () => clearTimeout(timerId);
+    }
+  });
+
+  const setGameState = (newCandidateDigits) => {
+    if (Logic.sum(newCandidateDigits) !== balls) {
+      setCandidateDigits(newCandidateDigits);
+    } else {
+      const newAvailableDigits = availableDigits.filter(n => !newCandidateDigits.includes(n));
+      setBalls(Logic.randomSumIn(newAvailableDigits, 9));
+      setAvailableDigits(newAvailableDigits);
+      setCandidateDigits([]);
+    }
+  };
+
+  return {
+    balls, setGameState, availableDigits, candidateDigits, secondsLeft,
+  };
+};
+
+const Game = ({ startNewGame }) => {
+  const {
+    balls,
+    availableDigits,
+    candidateDigits,
+    secondsLeft,
+    setGameState,
+  } = useGameState();
 
   const candidatesAreWrong = Logic.sum(candidateDigits) > balls;
-  const gameIsDone = availableDigits.length === 0;
+  const gameStatus = availableDigits.length === 0
+    ? 'won'
+    : secondsLeft === 0 ? 'lost' : 'active';
 
   const digitStatus = (digit) => {
     if (!availableDigits.includes(digit)) {
@@ -25,21 +63,14 @@ const Game = ({ startNewGame }) => {
   };
 
   const onDigitClick = (digit, currentStatus) => {
-    if (currentStatus === 'used') {
+    if (currentStatus === 'used' || secondsLeft === 0) {
       return false;
     }
 
     const newCandidateDigits = currentStatus === 'available'
       ? candidateDigits.concat(digit) : candidateDigits.filter(cn => cn !== digit);
 
-    if (Logic.sum(newCandidateDigits) !== balls) {
-      setCandidateDigits(newCandidateDigits);
-    } else {
-      const newAvailableDigits = availableDigits.filter(n => !newCandidateDigits.includes(n));
-      setBalls(Logic.randomSumIn(newAvailableDigits, 9));
-      setAvailableDigits(newAvailableDigits);
-      setCandidateDigits([]);
-    }
+    return setGameState(newCandidateDigits);
   };
 
   return (
@@ -47,14 +78,17 @@ const Game = ({ startNewGame }) => {
       <div className="header-title">Math Skillz</div>
       <div className="visuals">
         <div className="quiz-card left">
-          { gameIsDone ? <PlayAgain onClick={startNewGame} /> : <Balls count={balls} />}
+          { gameStatus !== 'active' ? <PlayAgain gameStatus={gameStatus} onClick={startNewGame} /> : <Balls count={balls} />}
         </div>
         <div className="quiz-card right">
           {Logic.range(1, 9)
             .map(number => <PlayDigit key={number} status={digitStatus(number)} number={number} onClick={onDigitClick} />)}
         </div>
       </div>
-      <div className="header-title">Timer</div>
+      <div className="header-title">
+        Time Remaining :
+        {secondsLeft}
+      </div>
     </div>
   );
 };
--- src/components/PlayAgain.js
+++ src/components/PlayAgain.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PlayAgain = ({ onClick }) => (
+const PlayAgain = ({ onClick, gameStatus }) => (
   <div className="game-done">
-    <button type="button" onClick={onClick}>Play Again</button>
+    <div
+      className="message"
+      style={{ color: gameStatus === 'lost' ? 'red' : 'green' }}
+    >
+      {gameStatus === 'lost' ? 'Game Over' : 'Nice'}
+    </div>
+    <button onClick={onClick}>Play Again</button>
   </div>
 );
 
 PlayAgain.propTypes = {
   onClick: PropTypes.func.isRequired,
+  gameStatus: PropTypes.string.isRequired,
 };
 
 export default PlayAgain;
